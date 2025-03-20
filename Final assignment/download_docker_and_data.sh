ed452e1089d2634cd80c1be7e0c80bf0f93ba0b9
VicBrienen <vic.brienen@gmail.com>
2025-03-20 15:41:59 +0100

    change compute time

--- Final assignment/download_docker_and_data.sh
+++ Final assignment/download_docker_and_data.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_mig
-#SBATCH --time=3:00:00
+#SBATCH --time=00:05:00
 
 # Pull container from dockerhub
 apptainer pull container.sif docker://cclaess/5lsm0:v1
