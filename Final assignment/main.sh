fe9c8fdc8544a946a6d4cc286735ac8078aba724
VicBrienen <vic.brienen@gmail.com>
2025-03-21 13:35:43 +0100

    change learning rate

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,10 +2,10 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 128 \
     --epochs 100 \
-    --lr 0.001 \
-    --weight-decay 0.01 \
+    --lr 0.0001 \
+    --weight-decay 0.0 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "unet-training" \
