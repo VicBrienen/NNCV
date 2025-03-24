702b7aba9631d7918ba2640f95fb2766bdd3430b
VicBrienen <vic.brienen@gmail.com>
2025-03-24 14:33:59 +0100

    adjust hyperparameters

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,8 +4,8 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
-    --lr 0.0001 \
-    --weight-decay 0.1 \
+    --lr 0.001 \
+    --weight-decay 0.0 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "transformer" \
+    --experiment-id "Unet" \
