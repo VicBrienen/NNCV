c3b29e2d814aa5a0bfc4e3a73ba38d1b6c446122
VicBrienen <vic.brienen@gmail.com>
2025-03-25 23:26:30 +0100

    fix resize labels

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -4,24 +4,18 @@ from transformers import SegformerForSemanticSegmentation
 
 class Model(SegformerForSemanticSegmentation):
     def __init__(self):
-        pretrained_model = "nvidia/segformer-b0-finetuned-ade-512-512"
-        model = SegformerForSemanticSegmentation.from_pretrained(pretrained_model)
-        
-        model.config.num_labels = 19
-        
-        model.classifier = torch.nn.Conv2d(model.config.hidden_sizes[-1], 19, kernel_size=1)
-        
-        super().__init__(model.config)
-        self.load_state_dict(model.state_dict(), strict=False)
-    
+        super().__init__.from_pretrained(
+            "nvidia/segformer-b0-finetuned-ade-512-512",
+            num_labels=19,
+            ignore_mismatched_sizes=True,
+        )
+
     def forward(self, pixel_values, **kwargs):
         outputs = super().forward(pixel_values, **kwargs)
         logits = outputs.logits
-        # Upsample logits to match input resolution
-        logits = F.interpolate(
+        return F.interpolate(
             logits,
-            size=pixel_values.shape[2:],  # matches the height and width of the input
+            size=pixel_values.shape[2:], 
             mode="bilinear",
             align_corners=False,
         )
-        return logits
