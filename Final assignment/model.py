009d6d81f3cba4fb8910263a163cc000def8959b
VicBrienen <vic.brienen@gmail.com>
2025-03-26 00:05:19 +0100

    b5 pretrained ade20k

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -6,7 +6,7 @@ class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.segformer = SegformerForSemanticSegmentation.from_pretrained(
-            "nvidia/segformer-b0-finetuned-ade-512-512",
+            "nvidia/segformer-b5-finetuned-ade-640-640",
             num_labels=19,
             ignore_mismatched_sizes=True
         )
