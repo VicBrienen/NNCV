99b24c21b5064591083056264363e20849792b19
VicBrienen <vic.brienen@gmail.com>
2025-03-25 23:21:57 +0100

    pretrained ade20k

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -1,21 +1,24 @@
 import torch
-from transformers import SegformerForSemanticSegmentation, SegformerConfig
+import torch.nn.functional as F
+from transformers import SegformerForSemanticSegmentation
 
 class Model(SegformerForSemanticSegmentation):
     def __init__(self):
-        config = SegformerConfig(
-            num_labels=19,
-            hidden_sizes=[64, 128, 320, 512],
-            depths=[3, 6, 40, 3],
-            num_heads=[1, 2, 5, 8],
-        )
-        super().__init__(config)
-
+        pretrained_model = "nvidia/segformer-b0-finetuned-ade-512-512"
+        model = SegformerForSemanticSegmentation.from_pretrained(pretrained_model)
+        
+        model.config.num_labels = 19
+        
+        model.classifier = torch.nn.Conv2d(model.config.hidden_sizes[-1], 19, kernel_size=1)
+        
+        super().__init__(model.config)
+        self.load_state_dict(model.state_dict(), strict=False)
+    
     def forward(self, pixel_values, **kwargs):
         outputs = super().forward(pixel_values, **kwargs)
         logits = outputs.logits
         # Upsample logits to match input resolution
-        logits = torch.nn.functional.interpolate(
+        logits = F.interpolate(
             logits,
             size=pixel_values.shape[2:],  # matches the height and width of the input
             mode="bilinear",
