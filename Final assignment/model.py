a1c70dd9a0f80038b98e3a2e5ceda8f617f5b66b
VicBrienen <vic.brienen@gmail.com>
2025-03-25 23:29:42 +0100

    fix attributeerror by wrapping

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -2,17 +2,17 @@ import torch
 import torch.nn.functional as F
 from transformers import SegformerForSemanticSegmentation
 
-class Model(SegformerForSemanticSegmentation):
+class Model(torch.nn.Module):
     def __init__(self):
-        super().__init__.from_pretrained(
+        super().__init__()
+        self.segformer = SegformerForSemanticSegmentation.from_pretrained(
             "nvidia/segformer-b0-finetuned-ade-512-512",
             num_labels=19,
-            ignore_mismatched_sizes=True,
+            ignore_mismatched_sizes=True
         )
 
     def forward(self, pixel_values, **kwargs):
-        outputs = super().forward(pixel_values, **kwargs)
-        logits = outputs.logits
+        logits = self.segformer(pixel_values).logits
         return F.interpolate(
             logits,
             size=pixel_values.shape[2:], 
