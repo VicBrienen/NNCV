354bde416fb9c56b189afcbc32912103a98840cb
VicBrienen <vic.brienen@gmail.com>
2025-04-01 21:44:41 +0200

    fix batchsize

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --annotation "coarse" \
-    --batch-size 32 \
+    --batch-size 16 \
     --epochs 30 \
     --lr 0.0001 \
     --weight-decay 0.01 \
