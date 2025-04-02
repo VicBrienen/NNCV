94b133e86166a68029bddbc10b2f9c18440ebbd3
VicBrienen <vic.brienen@gmail.com>
2025-04-02 20:31:32 +0200

    change jobscript

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -5,8 +5,8 @@ python3 train.py \
     --annotation "fine" \
     --resume-checkpoint "./checkpoints/b5 coarse/coarse_annotation_pretrained.pth" \
     --batch-size 8 \
-    --epochs 3 \
-    --lr 0.00006 \
+    --epochs 30 \
+    --lr 0.0001 \
     --weight-decay 0.01 \
     --num-workers 10 \
     --seed 42 \
