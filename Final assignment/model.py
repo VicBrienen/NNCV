6f2e70f0743e83067d49db13da060ab3a6c258de
VicBrienen <vic.brienen@gmail.com>
2025-04-03 00:28:55 +0200

    fix

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -12,9 +12,9 @@ class Model(torch.nn.Module):
 
     def forward(self, pixel_values, **kwargs):
         outputs = self.model(pixel_values=pixel_values)
-        masks_queries_logits = outputs.masks_queries_logits
+        class_queries_logits = outputs.class_queries_logits
         return torch.nn.functional.interpolate(
-            masks_queries_logits,
+            class_queries_logits,
             size=pixel_values.shape[2:],
             mode="bilinear",
             align_corners=False,
