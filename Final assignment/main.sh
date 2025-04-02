b7a81783b9cda8a7d259dd1d19e51ff95ec6ede8
VicBrienen <vic.brienen@gmail.com>
2025-04-02 19:21:29 +0200

    3 epochs lr=0.00006

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,8 +4,8 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --annotation "coarse" \
     --batch-size 8 \
-    --epochs 10 \
-    --lr 0.0001 \
+    --epochs 3 \
+    --lr 0.00006 \
     --weight-decay 0.01 \
     --num-workers 10 \
     --seed 42 \
