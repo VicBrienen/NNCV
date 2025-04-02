de5419b251adc83f968d12e1349102ff68d2f9db
VicBrienen <vic.brienen@gmail.com>
2025-04-02 15:22:05 +0200

    coarse dataset pretraining

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,11 +2,11 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --annotation "fine" \
+    --annotation "coarse" \
     --batch-size 8 \
-    --epochs 30 \
+    --epochs 10 \
     --lr 0.0001 \
     --weight-decay 0.01 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "segformer" \
+    --experiment-id "b5 coarse" \
