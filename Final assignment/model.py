520d73b6137c28ff105e9f8b34635e71481bd21f
VicBrienen <vic.brienen@gmail.com>
2025-04-07 15:07:37 +0200

    revert to segformer b5 ade 640 pretrained

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -5,11 +5,11 @@ class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.segformer = SegformerForSemanticSegmentation.from_pretrained(
-            "Vrjb/SegFormer_b3_mappillary_",
+            "nvidia/segformer-b5-finetuned-ade-640-640",
             num_labels=19,
             ignore_mismatched_sizes=True
         )
-
+        
     def forward(self, pixel_values, **kwargs):
         logits = self.segformer(pixel_values).logits
         return torch.nn.functional.interpolate(
