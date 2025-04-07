623ff33f7cb15e2dce705e2befc8e6e64ca876e9
VicBrienen <vic.brienen@gmail.com>
2025-04-07 15:41:54 +0200

    remove coarse annotation artifacts

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,12 +2,11 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --annotation "fine" \
-    --resume-checkpoint "./checkpoints/b5 coarse/coarse_annotation_pretrained.pth" \
+    --resume-checkpoint "None" \
     --batch-size 8 \
     --epochs 30 \
     --lr 0.0001 \
     --weight-decay 0.01 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "segformer b3" \
+    --experiment-id "segformer b5 ADE20k pretrained" \
--- Final assignment/train.py
+++ Final assignment/train.py
@@ -40,7 +40,6 @@ def get_args_parser():
 
     parser = ArgumentParser("Training script for a PyTorch U-Net model")
     parser.add_argument("--data-dir", type=str, default="./data/cityscapes", help="Path to the training data")
-    parser.add_argument("--annotation", type=str, default="fine", help="Annotation quality training set")
     parser.add_argument("--resume-checkpoint", type=str, default=None, help="Path to checkpoint for fine-tuning")
     parser.add_argument("--batch-size", type=int, default=64, help="Training batch size")
     parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
@@ -91,7 +90,7 @@ def main(args):
     train_dataset = Cityscapes(
         args.data_dir, 
         split="train", 
-        mode=args.annotation, 
+        mode="fine", 
         target_type="semantic", 
         transforms=train_transform
     )
