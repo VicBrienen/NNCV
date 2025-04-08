90c804a1ec09f962265af8e7ae4002410fabe25f
VicBrienen <vic.brienen@gmail.com>
2025-04-08 14:51:54 +0200

    implement warmup

--- Final assignment/train.py
+++ Final assignment/train.py
@@ -135,7 +135,13 @@ def main(args):
     optimizer = AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
     # poly decay
-    lr_lambda = lambda epoch: (1 - epoch / args.epochs) ** 0.9
+    def lr_lambda(epoch):
+        warmup_epochs = 3
+        if epoch < warmup_epochs:
+            return float(epoch + 1) / warmup_epochs
+        else:
+            return (1 - (epoch - warmup_epochs) / (args.epochs - warmup_epochs)) ** 0.9
+        
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
 
     # initialize AMP GradScaler
