7e65b8f182fc968c5ee1a0af5c14c71056a4c3f2
VicBrienen <vic.brienen@gmail.com>
2025-04-08 15:34:16 +0200

    new learning rate

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,10 +3,10 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --resume-checkpoint "None" \
-    --batch-size 2 \
-    --accumulation_steps 16 \
+    --batch-size 3 \
+    --accumulation_steps 4 \
     --epochs 30 \
-    --lr 0.0001 \
+    --lr 0.00006 \
     --weight-decay 0.01 \
     --num-workers 10 \
     --seed 42 \
