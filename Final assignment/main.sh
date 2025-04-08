365d2acb43b46bb8b281dcbfdb130d656cff447c
VicBrienen <vic.brienen@gmail.com>
2025-04-08 15:36:24 +0200

    test larger batchsize

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --resume-checkpoint "None" \
-    --batch-size 3 \
+    --batch-size 4 \
     --accumulation_steps 4 \
     --epochs 30 \
     --lr 0.00006 \
