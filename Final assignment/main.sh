e31d931958396b8ef1ed722e69b2a9ff40518c40
VicBrienen <vic.brienen@gmail.com>
2025-04-09 19:39:00 +0200

    num workers = 9 and batchsize =3

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,11 +3,11 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --resume-checkpoint "None" \
-    --batch-size 4 \
+    --batch-size 3 \
     --accumulation_steps 4 \
     --epochs 50 \
     --lr 0.00006 \
     --weight-decay 0.01 \
-    --num-workers 16 \
+    --num-workers 9 \
     --seed 42 \
     --experiment-id "segformer b5 ADE20k pretrained" \
