33ba434ca6a9d59fb7ba05ec7f6de905310f2556
VicBrienen <vic.brienen@gmail.com>
2025-04-09 17:51:12 +0200

    configure h100 setup and double check single gpu usage

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
-#SBATCH --gpus=1
-#SBATCH --partition=gpu_a100
-#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_h100
+#SBATCH --time=00:05:00
 
-srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
+# Run inside container, including diagnostics
+srun apptainer exec --nv --env-file .env container.sif /bin/bash -c "
+    echo 'CUDA_VISIBLE_DEVICES: '\$CUDA_VISIBLE_DEVICES
+    echo 'Running nvidia-smi inside container...'
+    nvidia-smi
+    echo 'Starting training script...'
+    /bin/bash main.sh
+"
