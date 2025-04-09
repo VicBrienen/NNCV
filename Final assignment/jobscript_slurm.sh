61c232fb88ede001846bc15bbfa9465b5e52f59e
VicBrienen <vic.brienen@gmail.com>
2025-04-09 17:58:24 +0200

    change batchsize

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -4,13 +4,6 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu_h100
-#SBATCH --time=00:05:00
+#SBATCH --time=10:00:00
 
-# Run inside container, including diagnostics
-srun apptainer exec --nv --env-file .env container.sif /bin/bash -c "
-    echo 'CUDA_VISIBLE_DEVICES: '\$CUDA_VISIBLE_DEVICES
-    echo 'Running nvidia-smi inside container...'
-    nvidia-smi
-    echo 'Starting training script...'
-    /bin/bash main.sh
-"
+srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
